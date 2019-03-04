e9710c1029e353dd87d99733b2fd9767207bc070
CharlesGe129 <char1209ge@gmail.com>
2019-03-04 13:27:01 -0800

    BlockChain.Show()

--- p2/BlockChain.go
+++ p2/BlockChain.go
@@ -1,1 +1,32 @@
 package p2
+
+import (
+	"encoding/hex"
+	"fmt"
+	"golang.org/x/crypto/sha3"
+	"sort"
+)
+
+func (bc *BlockChain) Show() string {
+	rs := ""
+	var idList []int
+	for id := range bc.Chain {
+		idList = append(idList, int(id))
+	}
+	sort.Ints(idList)
+	for _, id := range idList {
+		var hashs []string
+		for _, block := range bc.Chain[int32(id)] {
+			hashs = append(hashs, block.Header.Hash + "<=" + block.Header.ParentHash)
+		}
+		sort.Strings(hashs)
+		rs += fmt.Sprintf("%v: ", id)
+		for _, h := range hashs {
+			rs += fmt.Sprintf("%s, ", h)
+		}
+		rs += "\n"
+	}
+	sum := sha3.Sum256([]byte(rs))
+	rs = fmt.Sprintf("This is the BlockChain: %s\n", hex.EncodeToString(sum[:])) + rs
+	return rs
+}
